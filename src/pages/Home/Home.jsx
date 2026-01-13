b76503e8180e82f25d48b0579d66f85fa9ec4370
sabbirHossain <sabbirshnt@gmail.com>
2026-01-13 18:53:22 +0600

    done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -45,7 +45,7 @@ owIDAQAB
   return (
     <div className="h-screen w-full flex  items-center justify-center">
       <form className="flex flex-col gap-y-5" onSubmit={onSubmit}>
-        <input
+        <textarea
           className="border px-3 py-2 rounded h-[300px] w-[500px]"
           onChange={(e) => setText(e.target.value)}
           type="text"
