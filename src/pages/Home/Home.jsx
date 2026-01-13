ac6948c055a63dd5c5018c69760fcac3455135d0
sabbirHossain <sabbirshnt@gmail.com>
2026-01-13 12:44:00 +0600

    done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,9 +13,9 @@ const Home = () => {
     const publicKey = `MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1oPV6JCZUNzro3heK3II XmRp9LtFZvzzXBJK8l3U24tIZayZJ/ddcKf0jDR1tRW3mYnWRIuhF60kyOR5iDX3 qzosZkegnQ5L82JN0rq6HDtsdfLmB+pEfNkxTM8f5puE+RJeB80FTq0GpCj3sKs5 qCPvTqH2RHDIT0WcXT7DDLOjwAJJHoCVfvBqmMptHir1a1izUlIPXHPYuZ5EMpLu 9KEa5nEVVN+urW4uhYAgjMf/p+8ElcqkpdFNNHFsglSzPyt0cHtwQka0FjQuiX8x +XsrTOQHSOHz+NG6MUA2OB2Ay7YgtAq4JxbP0J6896QGv1wYwQTG74QWBy1NuolI owIDAQAB`;
 
     encryptor.setPublicKey(publicKey);
-    const encrypted = encryptor.encrypt(JSON.stringify({ data: text }));
+    const encrypted = encryptor.encrypt(JSON.stringify(text));
 
-    const { data } = await axios.post(API.decrypt, encrypted);
+    const { data } = await axios.post(API.decrypt, { token: encrypted });
     console.log(data);
   };
   return (
